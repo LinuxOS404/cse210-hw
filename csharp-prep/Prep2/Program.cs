ea365cdd8314fda0eedb2fa8c9348fa8986cba2a
LinuxOS404 <send2flash@gmail.com>
2026-01-14 00:40:47 -0700

    Prep2 Complete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,49 @@
 using System;
+using System.IO.Pipes;
+using System.Linq.Expressions;
+using System.Security.Cryptography;
 
-class Program
+Console.WriteLine("Please input your percentage here to determine your grade");
+string prcent = Console.ReadLine();
+int result = int.Parse(prcent);
+
+string grade = "";
+
+if (result >= 90)
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+    grade = "A";
+}
+else if (result >= 80)
+{
+    grade = "B";
+}
+
+else if (result >= 70)
+{
+    grade = "C";
+}
+
+else if (result >= 60)
+{
+    grade = "D";
+}
+
+
+else
+{
+    grade = "F";
+}
+
+if (result <= 70)
+{
+
+    Console.WriteLine($"Your grade is an:{grade}");
+}
+if (result <= 70)
+{
+    Console.WriteLine($"Your grade for this class was an {grade} You have failed this course,you will need to take this course again.");
+}
+else
+{
+    Console.WriteLine($"Your grade for this class was an {grade} You have passed this course.");
 }
