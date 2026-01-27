5f00c952ee2afa1bfbd321d740b12524b1bd591e
LinuxOS404 <send2flash@gmail.com>
2026-01-26 22:03:16 -0700

    Prep 3 finsihed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,42 @@
 using System;
 
-class Program
+string response = "yes";
+while (response == "yes")
 {
-    static void Main(string[] args)
+    do
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        Console.WriteLine("What is the magic number?");
+        int Magicnum = int.Parse(Console.ReadLine()); ;
+
+
+
+        int Guessnum = 0;
+        int tries = 0;
+        do
+        {
+            Console.WriteLine("What is your guess?");
+            Random randomGenerator = new Random();
+            Guessnum = randomGenerator.Next(1, 10);
+            Console.WriteLine($"The generator number guessed was: {Guessnum}");
+            
+
+        tries++;
+            if (Guessnum < Magicnum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (Guessnum > Magicnum)
+            {
+                Console.WriteLine("Lower");
+            }
+
+        } while (Guessnum != Magicnum);
+
+        Console.WriteLine("Correct!");
+        Console.WriteLine($"It took your computer {tries} {(tries == 1 ? "try" : "tries to get it right")}");
+
+        Console.Write("Do you want to continue? (yes/no): ");
+        response = Console.ReadLine().ToLower();
+    } while (response == "yes");
 }
+
--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        
+        {
+        Job job1 = new Job();
+        job1._jobTitle = "Customer support";
+        job1._company = "Roi Solutions";
+        job1._startYear = 2018;
+        job1._endYear = 2021;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Cook";
+        job2._company = "Via 313";
+        job2._startYear = 2021;
+        job2._endYear = 2022;
+
+        Resume myResume = new Resume();
+        myResume._name = "Celine Heaton";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+        }
+
     }
 }
