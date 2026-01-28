d02299e84e174fda08ce660dc154ccde4559b0c7
LinuxOS404 <send2flash@gmail.com>
2026-01-27 21:32:23 -0700

    Prep4 Complete

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.Collections.Generic;
 
-class Program
+
+Console.WriteLine("Enter a list of numbers, type 0 when finished!");
+
+
+List<int> numbers = new List<int>();
+int input;
+
+
 {
-    static void Main(string[] args)
+    Console.WriteLine("Enter number:");
+    do
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        Console.Write("Enter a number (0 to stop): ");
+        input = int.Parse(Console.ReadLine());
+
+    if (input != 0)
+    {
+        numbers.Add(input);
     }
+
+} while (input != 0);
+
+int sum = 0;
+foreach (int num in numbers)
+{
+    sum += num;
+}
+
+double average = (double)sum / numbers.Count;
+int max = numbers[0];
+
+foreach (int num in numbers)
+{
+    if (num > max)
+    {
+        max = num;
+    }
+}
+
+Console.WriteLine($"The Sum is: {sum}");
+Console.WriteLine($" TheAverage is: {average}");
+Console.WriteLine($"The Largest number is: {max}");
 }
