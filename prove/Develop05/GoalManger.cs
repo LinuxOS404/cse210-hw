c938c1d277f2ab70b1264bfa900121cf139c3c90
LinuxOS404 <send2flash@gmail.com>
2026-04-06 22:30:45 -0600

    Goals done

--- prove/Develop05/GoalManger.cs
+++ prove/Develop05/GoalManger.cs
@@ -1,6 +1,211 @@
-public class GoalManager : Goal
-{
-    public GoalManager(string _name, int _points, bool _iscomplete) : base(_name, _points, _iscomplete)
+
+
+using System.IO;
+
+    public class GoalManager 
     {
+        private List<Goal> goalList = new List<Goal>();
+        private int totalPoints;
+        private int _level;
+
+
+        
+        public void UpdateLevel()//updates the level determined by the amount of points the user has = the extra stuff
+        {
+            if (totalPoints >= 5000)
+            {
+                _level = 5; 
+            }
+            else if (totalPoints >= 3000)
+            {
+                _level = 4; 
+            }
+            else if (totalPoints >= 1500)
+            {
+                _level = 3; 
+            }
+            else if (totalPoints >= 500)
+            {
+                _level = 2; 
+            }
+            else
+            {
+                _level = 1; 
+            }
+        }
+
+        
+        public void SaveGoals()//save the goals
+        {
+            string fileName = AskForFileName();
+            
+            using (StreamWriter outputFile = new StreamWriter(fileName))
+            {
+               
+                outputFile.WriteLine($"Points,{totalPoints}");
+                outputFile.WriteLine($"Level,{_level}");
+
+                foreach (Goal goal in goalList)
+                {
+                    
+                    outputFile.WriteLine(goal.GetGoalType());
+                }
+            }
+            Console.WriteLine("Progress saved successfully!");
+        }
+
+        
+        public void LoadGoals()//loads the goals and the type, name and description.
+        {
+            string fileName = AskForFileName();
+            
+            if (File.Exists(fileName))
+            {
+                string[] lines = File.ReadAllLines(fileName);
+                goalList.Clear();
+
+                foreach (string line in lines)
+                {
+                    string[] goalParts = line.Split(',');
+                    string typeLabel = goalParts[0];
+
+                    if (typeLabel == "Points")
+                    {
+                        totalPoints = int.Parse(goalParts[1]);
+                    }
+                    else if (typeLabel == "Level")
+                    {
+                        _level = int.Parse(goalParts[1]);
+                    }
+                    else if (typeLabel == "SimpleGoal")
+                    {
+                        
+                        string name = goalParts[1];
+                        int points = int.Parse(goalParts[2]);
+                        string description = goalParts[3];
+                        bool isComplete = bool.Parse(goalParts[4]);
+                        
+                        goalList.Add(new Simple(name, points, description, isComplete));
+                    }
+                    else if (typeLabel == "EternalGoal")
+                    {
+                        goalList.Add(new Eternal(goalParts[1], int.Parse(goalParts[2]), goalParts[3], false));
+                    }
+                    else if (typeLabel == "ChecklistGoal")
+{
+    
+    string name = goalParts[1];
+    int points = int.Parse(goalParts[2]);
+    string description = goalParts[3];
+    bool isComplete = bool.Parse(goalParts[4]);
+    int bonus = int.Parse(goalParts[5]);
+    int maxCompletions = int.Parse(goalParts[6]);
+
+    
+    CheckList checklistGoal = new CheckList(name, points, description, isComplete, bonus, maxCompletions);
+    
+    
+    checklistGoal.SetCurrentCount(int.Parse(goalParts[7]));
+    
+    goalList.Add(checklistGoal);
+}
+                }
+                Console.WriteLine("Goals loaded. Current level: " + _level);
+            }
+            else
+            {
+                Console.WriteLine("File not found.");
+            }
+        }
+
+        private string AskForFileName()
+        {
+            Console.Write("Enter the filename for the quest file: ");
+            return Console.ReadLine();
+        }
+
+        public int GetLevel()
+{
+    return _level;   
+}
+
+
+public int GetTotalPoints()//Gets the whole amount of points a user has
+{
+    return totalPoints;
+}
+
+public void CreateGoal()//creates a goal aka the goal menu one #1 is selected.
+{
+    Console.WriteLine("\nThe types of Goals are:");
+    Console.WriteLine("  1. Simple Goal");
+    Console.WriteLine("  2. Eternal Goal");
+    Console.WriteLine("  3. Checklist Goal");
+    Console.Write("Which type of goal would you like to create? ");
+    string choice = Console.ReadLine();
+
+    Console.Write("What is the name of your goal? "); string name = Console.ReadLine();
+    Console.Write("What is a short description of it? "); string description = Console.ReadLine();
+    Console.Write("What is the amount of points associated with this goal? "); int points = int.Parse(Console.ReadLine());
+
+    if (choice == "1")//simple goal
+    {
+        goalList.Add(new Simple(name, points, description, false));
+    }
+    else if (choice == "2")//eternal goal
+    {
+        goalList.Add(new Eternal(name, points, description, false));
+    }
+    else if (choice == "3")//checklist goal
+    {
+        Console.Write("How many times does this goal need to be accomplished for a bonus? ");
+        int max = int.Parse(Console.ReadLine());
+        Console.Write("What is the bonus for accomplishing it that many times? ");
+        int bonus = int.Parse(Console.ReadLine());
+        
+        goalList.Add(new CheckList(name, points, description, false, bonus, max));
     }
 }
+
+public void ListGoalDetails()//Show what goals there are
+{
+    Console.WriteLine("The goals are:");
+
+    
+    if (goalList.Count == 0)
+    {
+        Console.WriteLine("No goals created yet.");
+    }
+    else
+    {
+        
+        for (int i = 0; i < goalList.Count; i++)
+        {
+            
+            
+            Console.WriteLine($"{i + 1}. {goalList[i].GetDetailsString()}");
+        }
+    }
+}
+
+
+public void RecordEvent()//calls RecordEvent method 
+{
+    
+    ListGoalDetails(); 
+
+    Console.Write("Which goal did you accomplish? ");
+    int goalIndex = int.Parse(Console.ReadLine()) - 1;
+
+    
+    int pointsEarned = goalList[goalIndex].RecordEvent();
+
+    
+    totalPoints += pointsEarned;
+
+    Console.WriteLine($"Congratulations! You earned {pointsEarned} points!");
+    Console.WriteLine($"You now have {totalPoints} points.");
+}
+
+    }
+
--- prove/Develop05/Program.cs
+++ prove/Develop05/Program.cs
@@ -1,35 +1,28 @@
 using System;
-using System.ComponentModel.DataAnnotations;
-using System.Security.Cryptography.X509Certificates;
-class Program : Goal
+class Program 
 {
-    public Program(string _name, int _points, bool _iscomplete) : base(_name, _points, _iscomplete)
-    {
-    }
-
     static void Main(string[] args)
     {
         Console.WriteLine("Eternal Quest");
 
-
-while(true)
-        {
-
-        Goal goals = null;
-
-        PublicKey void DisplayScore()
-            {
-             return ($"You have {PointCalculation()}points;   
-            }
+        GoalManager manager = new GoalManager();
         
-        Console.WriteLine($"{DisplayScore()}");
+        Console.WriteLine("Welcome to the Eternal Quest Program!");
 
+        bool running = true;
+        while(running)
+        {
+            //Show the points and level of the user = the extra thing of this program to get full points
+            manager.UpdateLevel();
+            Console.WriteLine($"\nYou have {manager.GetTotalPoints()} points.");
+            Console.WriteLine($"Current Level: {manager.GetLevel()}");
+
+//show the menu to the user
         Console.WriteLine("1.Create new Goal ");
         Console.WriteLine("2. See Existing Goals ");
         Console.WriteLine("3. Save Goals ");
         Console.WriteLine("4. Load Goal");
         Console.WriteLine("5. Record Event");
-        Console.WriteLine("6. Check my Level");
 
         Console.WriteLine("7. Quit");
 
@@ -38,11 +31,37 @@ while(true)
 
         string choice = Console.ReadLine();
 
+        //Use the methods attched to the option
+        if (choice == "1")//creates a goal and shows user the Goal menu.
+        {
+            manager.CreateGoal(); 
         }
-
-
-
-
-
+        else if (choice == "2") //show the user the goals they already have.
+        {
+            manager.ListGoalDetails(); 
+        }
+        else if (choice == "3")//save the user's goals
+        {
+            manager.SaveGoals(); // 
+        }
+        else if (choice == "4")//Load the user's goals
+        {
+            manager.LoadGoals(); 
+        }
+        else if (choice == "5")//Record an event base on which goal was achieved.
+        {
+            manager.RecordEvent();
+        }
+        
+        else if (choice == "6")//stop program so user can quit
+        {
+            running = false; 
+        }
+    } 
 }
-}
+} 
+
+
+        
+
+
